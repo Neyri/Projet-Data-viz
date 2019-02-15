66171e47c86ba7a90a59c21d4287a29e5c0be804
Neyri <martin-chauvin@laposte.net>
2019-02-15 17:49:01 +0100

    More filtering

--- scripts/filter.py
+++ scripts/filter.py
@@ -1,7 +1,16 @@
 import json
 import datetime
 
+to_delete = ['IN_VEHICLE', 'IN_FOUR_WHEELER_VEHICLE',
+             'IN_TWO_WHEELER_VEHICLE']
+to_merge = {
+    'ON_FOOT': 'WALKING',
+    'TITLING': 'STILL'
+}
+
+
 file = 'data\Historique des positions.json'
+
 
 with open(file) as f:
     data = json.load(f)
@@ -11,11 +20,26 @@ data = data['locations']
 data_to_keep = []
 for d in data:
     if 'activity' in d.keys():
-        # print(d)
         timestamp = int(d['timestampMs'])
         date = datetime.datetime.fromtimestamp(timestamp / 1e3)
         if date.year >= 2018:
             data_to_keep.append(d)
+            # Filter the activities
+            activity = d['activity'][0]['activity']
+            activity_final = []
+            types = []
+            sum = 0
+            for act in activity:
+                if act['type'] not in to_delete:
+                    if act['type'] in to_merge.keys():
+                        act['type'] = to_merge[act['type']]
+                    if act['type'] not in types:
+                        activity_final.append(act)
+                        types.append(act['type'])
+                        sum += act['confidence']
+            for act in activity_final:
+                act['confidence'] = act['confidence'] / sum * 100
+            d['activity'] = activity_final
 
 with open('data\data.json', 'w') as outfile:
     json.dump(data_to_keep, outfile)
