fd7f7233432ea419930d58a2565296439f54a96d
shtheo <theo.ponton@gmail.com>
2019-03-04 16:53:00 +0100

    no nan and normalization

--- scripts/activity_distance.js
+++ scripts/activity_distance.js
@@ -2,7 +2,6 @@ function build_distances_acti(daily_locations, color) {
   d3.select("#probas_actis_distance").remove()
   daily_activities = get_daily_activities(daily_locations);
 
-  var legend_width = 0;
   var margin = {
     top: 20,
     right: 20,
@@ -10,12 +9,12 @@ function build_distances_acti(daily_locations, color) {
     left: 30
   };
   var inner_width = 500 // d3.select("#div_distance_acti").node().getBoundingClientRect().width
-  var width = inner_width - margin.left - margin.right - legend_width,
+  var width = inner_width - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
   var svg = d3.select("#div_distance_acti").append("svg")
     .attr("id", "probas_actis_distance")
-    .attr("width", width + margin.left + margin.right + legend_width)
+    .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -32,15 +31,15 @@ function build_distances_acti(daily_locations, color) {
     'EXITING_VEHICLE': 0
   }
   for (var i = 0; i < (daily_locations.length - 1); i++) {
-    if (daily_locations[i].activity[0].type == daily_locations[i + 1].activity[0].type) {
       lat1 = daily_locations[i].lat * Math.PI / 180
       lat2 = daily_locations[i + 1].lat * Math.PI / 180
       long1 = daily_locations[i].long * Math.PI / 180
       long2 = daily_locations[i + 1].long * Math.PI / 180
-      distance = Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(long2 - long1)) * 6371
-      distances[daily_locations[i].activity[0].type] = distances[daily_locations[i].activity[0].type] + distance
-      distances['TOTAL'] = distances['TOTAL'] + distance
-    }
+	  if (lat1 != lat2 || long1 != long2){
+		  distance = Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(long2 - long1)) * 6371
+		  distances[daily_locations[i].activity[0].type] = distances[daily_locations[i].activity[0].type] + distance
+		  distances['TOTAL'] = distances['TOTAL'] + distance
+	  }
   }
   console.log(distances);
   var offsety = 0;
@@ -48,7 +47,7 @@ function build_distances_acti(daily_locations, color) {
     svg.append("rect")
       .attr("x", 180)
       .attr("y", 30 + offsety)
-      .attr("width", Math.max(4 * distances[activity], 2.5))
+      .attr("width", Math.max((inner_width - 180)/distances['TOTAL'] * distances[activity], 2.5))
       .attr("height", 18)
       .style("fill", color(activity));
     svg.append("text")
